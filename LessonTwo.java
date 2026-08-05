7f7bf96d9ded6f13bb519d0e3c8af3b9de3a9485
Anirban <85406733+Anirban9372@users.noreply.github.com>
2026-08-06 01:00:42 +0530

    Understanding of math functions and what static does to class variables outside main function

--- LessonTwo.java
+++ LessonTwo.java
@@ -2,11 +2,24 @@ import java.util.Scanner;
 
 public class LessonTwo {
     static Scanner userInput = new Scanner(System.in);
+    // static is defined and new is also defined as a new Scanner
+    // object is created and assigned to the variable userInput.
+    // Since it is not defined under main class so to get called by
+    // main class which is static we also have to call the new
+    // scanner with a static variable too.
 
     public static void main(String[] args) {
         System.out.println("Please enter a number: ");
-        if (userInput.hasNextInt()) {
-
+        if (userInput.hasNextInt()) { // is the next thing typed in an integer?
+            int numberEntered = userInput.nextInt(); // it reads the integer and stores in numberEntered
+            System.out.println("You entered: " + numberEntered);
+            int nabs = Math.abs(numberEntered);
+            int max = Math.max(nabs, numberEntered);
+            System.out.println("The absolute value of the number you entered is: " + nabs);
+            System.out.println("The maximum value of the number you entered is: " + max);
+        } else {
+            System.out.println("You didn't enter an integer!!");
         }
+
     }
 }
